f56c260628a39582e3aaf885f30d1a764fd46c42
Pace <twswe@gmail.com>
2026-05-17 05:12:33 +0800

    Refresh boke settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, sessionDrivers } from 'astro/config';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough'
+  }),
+  session: {
+    driver: sessionDrivers.lruCache()
+  },
   vite: {
     plugins: [tailwindcss()]
   }
